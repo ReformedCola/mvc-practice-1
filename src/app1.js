34c5da80b01f328a62c3a68fbcd99a26a69ea5bd
ReformedCola <reformedcola@gmail.com>
2020-03-28 14:59:50 -0700

    Rewrite App1 & App2 with Vue.js

--- src/app1.js
+++ src/app1.js
@@ -1,62 +1,44 @@
 import "./app1.css";
-import $ from "jquery";
-import Model from './base/Model'
-import View from './base/View'
+import Vue from 'vue'
 
-// data for m
-const m = new Model({
-  data: {
-    n: parseFloat(localStorage.getItem('n'))
-  },
-  update(data){
-    Object.assign(m.data, data)
-    m.trigger('m:updated')
-    localStorage.setItem('n', m.data.n)
-  }
-})
 
 // others for c
 const init = (el) => {
-  new View( {
-    el: el,
-    data: m.data,
-    html: `
-      <div>
+  new Vue({
+    el:el,
+    data: {n: parseFloat(localStorage.getItem('n'))},
+    methods: {
+      add() {
+        this.n += 1
+      },
+      minus() {
+        this.n -= 1
+      },
+      mul() {
+        this.n *= 2
+      },
+      div() {
+        this.n /= 2
+      }
+    },
+    watch: {
+      n: function() {
+        localStorage.setItem('n', this.n)
+      }
+    },
+    template: `
+      <section>
         <div class="output">
           <span id="number">{{n}}</span>
         </div>
         <div class="actions">
-          <button id="add1">+1</button>
-          <button id="minus1">-1</button>
-          <button id="mul2">*2</button>
-          <button id="divide2">÷2</button>
+          <button @click="add">+1</button>
+          <button @click="minus">-1</button>
+          <button @click="mul">*2</button>
+          <button @click="div">÷2</button>
         </div>
-      </div>
-    `,
-    render(data) {
-      const n = data.n
-      if(this.el.children.length !== 0) this.el.empty()
-      $(this.html.replace('{{n}}', n))
-        .appendTo(this.el)
-    },
-    events: {
-      'click #add1': 'add',
-      'click #minus1': 'minus',
-      'click #mul2': 'mul',
-      'click #divide2': 'div'
-    },
-    add() {
-      m.update({n: m.data.n + 1})
-    },
-    minus() {
-      m.update({n: m.data.n - 1})
-    },
-    mul() {
-      m.update({n: m.data.n * 2})
-    },
-    div() {
-      m.update({n: m.data.n / 2})
-    }
+      </section>
+    `
   })
 }
 
--- src/app2.js
+++ src/app2.js
@@ -1,50 +1,31 @@
 import "./app2.css";
-import $ from "jquery"
-import Model from './base/Model'
-import View from './base/View'
-
-const localKey = 'app2.index'
-const m = new Model ({
-  data: {
-    index: parseInt(localStorage.getItem(localKey)) || 0
-  },
-  update(data){
-    Object.assign(m.data, data)
-    m.trigger('m:updated')
-    localStorage.setItem('app2.index', m.data.index)
-  },
-})
+import Vue from 'vue'
 
 const init = (el) => {
-  new View({
+  new Vue({
     el: el,
-    data: m.data,
-    html: (index) => {
-      return `
-    <div>
-      <ol class="tab-bar">
-        <li class="${index === 0 ? 'selected' : ''}" data-index="0"><span>1111</span></li>
-        <li class="${index === 1 ? 'selected' : ''}" data-index="1"> <span>2222</span></li>
-      </ol>
-      <ol class="tab-content">
-        <li class="${index === 0 ? 'active' : ''}">内容1</li>
-        <li class="${index === 1 ? 'active' : ''}">内容2</li>
-      </ol>
-    </div>
+    data: {
+      index: parseInt(localStorage.getItem('index')) || 0
+    },
+    watch: {
+      index: function() {
+        localStorage.setItem('index', this.index)
+      }
+    },
+    template: `
+      <section id="app2">
+        <ol class="tab-bar">
+          <li :class="index === 0 ? 'selected' : ''"
+              @click="index=0"><span>1111</span></li>
+          <li :class=" index === 1 ? 'selected' : ''"
+              @click="index=1"><span>2222</span></li>
+        </ol>
+        <ol class="tab-content">
+          <li :class="index === 0 ? 'active' : ''">内容1</li>
+          <li :class="index === 1 ? 'active' : ''">内容2</li>
+        </ol>
+      </section>
     `
-    },
-    render(data) {
-      const index = data.index
-      if (this.el.children.length !== 0) this.el.empty()
-      $(this.html(index)).appendTo(this.el)
-    },
-    events: {
-      'click .tab-bar li': 'x',
-    },
-    x(e) {
-      const index = parseInt(e.currentTarget.dataset.index)
-      m.update({index: index})
-    }
   })
 }
 
